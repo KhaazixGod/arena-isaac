3aab3e70081b7321696f3e245de7766e21847f47
Vova Shcherbyna <dev@voshch.dev>
2025-12-02 14:12:48 +0100

    fix trailing slashes in world_path

--- arena_isaac/isaac_utils/utils/path.py
+++ arena_isaac/isaac_utils/utils/path.py
@@ -3,12 +3,12 @@ import re
 
 
 def sanitize_path_component(component: str) -> str:
-    if not re.match(r'^[a-zA-Z_]', component):
+    if component and not re.match(r'^[a-zA-Z_]', component):
         return f'_{component}'
     return component
 
 
 def world_path(*path: str) -> str:
     if len(path) == 1:
-        path = path[0].split(os.sep)
-    return os.path.join('/World', *map(sanitize_path_component, path))
+        path = tuple(path[0].split(os.sep))
+    return os.path.join('/World', *filter(None, map(sanitize_path_component, path)))
